84267f01d4a0199903f4a3bd0fe26c2243755fd7
Vignesh <83863784+0xevilox@users.noreply.github.com>
2022-08-23 23:01:28 +0530

    Add files via upload

---
+++ Successchecker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Checking the success by Luck"
+sleep 5
+echo "What is your name ?"
+read name
+sleep 2
+echo "what is your age ?"
+read age
+sleep 1
+echo "So Let's us find when you reach your success randomly"
+sleep 2
+echo "loading................................................................."
+sleep 3
+success=$((($RANDOM % 15) + $age))
+sleep 2
+echo "Loading ........................"
+sleep 1
+echo "Just wait 3.....................2.......................1........0"
+sleep 4
+echo "boooooooooooooooommmmmmmmmmmmmmmmmmmmmmmmmm..........................."
+sleep 1
+echo "At age the of $success you will reach your success"
+sleep 2
+echo "It just for fun don't wait for success just creat it you will find the magic...."
